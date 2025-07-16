71fb100eab2e1c4296fdc70b8401b83f15dcdca7
dfntlygeorge <g.a.donayre.business@gmail.com>
2025-07-16 21:03:28 +0800

    feat: calculate left macros for today

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,50 +23,39 @@ class DashboardController extends Controller
     public function index(): View
     {
         $user = User::first();
-
+        $meals = Meal::where('user_id', $user->id)
+            ->whereDate('date', Carbon::today())
+            ->get();
+        $total_calories = $meals->sum('total_calories');
+        $total_protein = $meals->sum('protein');
+        $total_carbs = $meals->sum('carbs');
+        $total_fat = $meals->sum('fat');
         $daily_macros_target = [
             [
                 'label' => 'Calorie',
                 'daily_target' => $user->daily_calorie_target,
                 'unit' => 'kcal',
-                'left' => 300, // HARDCODED FOR NOW
+                'left' => max($user->daily_calorie_target - $total_calories, 0),
             ],
             [
                 'label' => 'Protein',
                 'daily_target' => $user->daily_protein_target,
                 'unit' => 'g',
-                'left' => 24, // HARDCODED FOR NOW
+                'left' => max($user->daily_protein_target - $total_protein, 0)
             ],
             [
                 'label' => 'Carbs',
                 'daily_target' => $user->daily_carbs_target,
                 'unit' => 'g',
-                'left' => 36, // HARDCODED FOR NOW
+                'left' => max($user->daily_carbs_target - $total_carbs, 0)
             ],
             [
                 'label' => 'Fat',
                 'daily_target' => $user->daily_fat_target,
                 'unit' => 'g',
-                'left' => 12, // HARDCODED FOR NOW
+                'left' => max($user->daily_fat_target - $total_fat, 0)
             ],
         ];
-
-        $meals = Meal::where('user_id', $user->id)->get();
-
-        // $mealDescription = "I ate 200g of chicken pastil all chicken breast meat with 150g of red rice.";
-
-        // $macros = $this->macroService->analyze($mealDescription);
-
-        // Meal::create([
-        //     'user_id' => $user->id,
-        //     'description' => $macros['description'],
-        //     'total_calories' => $macros['total_calories'],
-        //     'protein' => $macros['protein'],
-        //     'carbs' => $macros['carbs'],
-        //     'fat' => $macros['fat'],
-        //     'date' => Carbon::today(),
-        // ]);
-
 
 
         return view("dashboard.index", compact('user', 'daily_macros_target', 'meals'));
