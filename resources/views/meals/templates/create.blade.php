51e18561e5da4c23ed4c688597c6653766cb8218
dfntlygeorge <g.a.donayre.business@gmail.com>
2025-07-24 12:37:54 +0800

    added dynamic app title

--- resources/views/meals/templates/create.blade.php
+++ resources/views/meals/templates/create.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <x-slot name="title">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Create Meal Template
-        </h2>
+
+        Create Meal Template
+
     </x-slot>
 
     <div class="min-h-screen bg-gray-900 text-gray-100">
